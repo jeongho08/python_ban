b1bc4d3b4f87b76b69cae0975f8c67ae1b634510
jeongho08 <hwangjeongho0807@gmail.com>
2021-09-03 17:06:04 +0900

    중급반 test 내용

---
+++ 중급반_test.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+# 중급반 test answer
+# 최소 금요일 9시 전까지 선생님에게 전송할 것.
+
+# 1번 문제
+# 주제: 문자열 입출력 + 연산자
+
+
+# 2번 문제
+# 주제: 조건문
+A = int(input("90>>> "))
+B = int(input("80>>> "))
+C = int(input("70>>> "))
+D = int(input("60>>> "))
+F = int(input(">>> "))
+
+
+if 95>=:
+    print("95점 축하합니다")
+else:
+    print("보충 대상자입니다. 당신의 방학은 이제 제껍니다 ㅎㅎ")
+
+# 3번 문제
+# 주제:리스트
+champion = ['티모', '아리', '리신', '람머스', '바드', '마스터 이']
+print(champion)
+print(type(champion))
+
+# 4번 문제
+# 주제: for 반복문(2중 for문) - 어려움
+
+
+# 5번 문제
+# 주제: while 반복문
+
+
+# 6번 문제
+# 주제: 함수
+
+
+# 7번 문제
+# 주제: 클래스
+class Dog():
+    def bark(self):
+        print("멍멍")
+    def attack(self):
+        print("크르릉..컹컹!")
+    def translate_bark(self):
+        print("멍멍")
+    def translate_attack(self):
+        print("크르릉..컹컹")
+
+
+
+
